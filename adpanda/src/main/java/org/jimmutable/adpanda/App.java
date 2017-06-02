0b2fba2e639060d493fa10730a232a01b90ae4ad
Jim Kane <j>
2017-06-02 13:07:06 -0700

    Inital code creation

---
+++ adpanda/src/main/java/org/jimmutable/adpanda/App.java
@@ -0,0 +1,13 @@
+package org.jimmutable.adpanda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
